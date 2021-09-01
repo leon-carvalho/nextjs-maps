975cd39752ab4fb45d2b35544195abd5a29cf98f
leon-carvalho <leonardoscc15@gmail.com>
2021-09-01 08:18:19 -0300

    Update app title and description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,14 +7,14 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>NextJS - Boilerplate</title>
+        <title>NextJS - Maps</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="A simple study project to practice NextJS, Leaflet and GraphQL"
         />
       </Head>
       <GlobalStyles />
