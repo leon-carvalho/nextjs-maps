d1d8b065ba721103b2b1d95fe3442bf580e320a8
leon-carvalho <leonardoscc15@gmail.com>
2021-09-01 08:15:00 -0300

    Clean Main componente
    Remove unused code from template

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,23 +1,5 @@
-import * as S from './styles'
-
 const Main = () => {
-  return (
-    <S.Wrapper>
-      <S.Logo
-        src="/img/logo.svg"
-        alt="Imagem de um átomo e React Avançado escrito ao lado"
-      />
-      <S.Title>NextJS Boilerplate</S.Title>
-      <S.Description>
-        TypeScript, ReactJS, Styled Components, Jest, Testing Library
-      </S.Description>
-
-      <S.Illustration
-        src="/img/hero-illustration.svg"
-        alt="Um desenvolvedor de frente para uma tela com código"
-      />
-    </S.Wrapper>
-  )
+  return <h1>Main</h1>
 }
 
 export { Main }
