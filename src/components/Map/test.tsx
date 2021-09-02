78e4b71378f3f46c2ba21fd7e613dfb1f7c16db4
leon-carvalho <leonardoscc15@gmail.com>
2021-09-02 09:07:37 -0300

    Test if Map render correctly without any marker

---
+++ src/components/Map/test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+
+import Map from '.'
+
+describe('<Main />', () => {
+  it('should render without any marker', () => {
+    render(<Map />)
+
+    expect(
+      screen.getByRole('link', {
+        name: /a js library for interactive maps/i
+      })
+    ).toBeInTheDocument()
+  })
+})
