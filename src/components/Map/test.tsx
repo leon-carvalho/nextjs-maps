c42c6671d42e706a34accf535588f59c87315a0b
leon-carvalho <leonardoscc15@gmail.com>
2021-09-03 08:30:25 -0300

    Test if map render with the marker in correct place

--- src/components/Map/test.tsx
+++ src/components/Map/test.tsx
@@ -12,4 +12,31 @@ describe('<Main />', () => {
       })
     ).toBeInTheDocument()
   })
+
+  it('should render with the marker in correct place', () => {
+    const place = {
+      id: '1',
+      name: 'Trindade',
+      slug: 'trindade',
+      location: {
+        latitude: 0,
+        longitude: 0
+      }
+    }
+
+    const placeTwo = {
+      id: '2',
+      name: 'Goiânia',
+      slug: 'goiania',
+      location: {
+        latitude: 0,
+        longitude: 0
+      }
+    }
+
+    render(<Map places={[place, placeTwo]} />)
+
+    expect(screen.getByTitle(/trindade/i)).toBeInTheDocument()
+    expect(screen.getByTitle(/goiânia/i)).toBeInTheDocument()
+  })
 })
