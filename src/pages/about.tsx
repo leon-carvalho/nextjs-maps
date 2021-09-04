a2db13b4568192432ba9c12d9ac985f4170e413d
leon-carvalho <leonardoscc15@gmail.com>
2021-09-04 19:18:05 -0300

    Use getStaticPaths to get pages slug

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,25 @@
+import client from 'graphql/client'
+import { GET_PAGES } from 'graphql/queries'
+import { GetStaticPaths } from 'next'
+import { useRouter } from 'next/dist/client/router'
 import AboutTemplate from 'templates/About'
 
-const About = (): JSX.Element => {
+export default function About(): JSX.Element {
+  const router = useRouter()
+
+  if (router.isFallback) return <small>Loading...</small>
+
   return <AboutTemplate />
 }
 
-export default About
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { pages } = await client.request(GET_PAGES, { first: 3 })
+
+  const paths = pages.map(({ slug }: { slug: string }) => ({
+    params: { slug }
+  }))
+
+  console.log(paths)
+
+  return { paths, fallback: true }
+}
