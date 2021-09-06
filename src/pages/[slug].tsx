30d01a7aaf43fcdfb6c9dafad8cd1518d6bade33
leon-carvalho <leonardoscc15@gmail.com>
2021-09-06 08:37:53 -0300

    Get page body and heading using GetStaticProps

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,6 +1,6 @@
 import client from 'graphql/client'
-import { GET_PAGES } from 'graphql/queries'
-import { GetStaticPaths } from 'next'
+import { GET_PAGES, GET_PAGE_BY_SLUG } from 'graphql/queries'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import { useRouter } from 'next/dist/client/router'
 import AboutTemplate from 'templates/About'
 
@@ -22,4 +22,19 @@ export const getStaticPaths: GetStaticPaths = async () => {
   console.log(paths)
 
   return { paths, fallback: true }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const page = await client.request(GET_PAGE_BY_SLUG, {
+    slug: params?.slug
+  })
+
+  if (!page) return { notFound: true }
+
+  return {
+    props: {
+      heading: page.heading,
+      body: page.body.html
+    }
+  }
 }
